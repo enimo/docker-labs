6acef6b39e2a79c7d45801341e25a1921507256d
enimo <enimong@gmail.com>
2022-07-03 14:57:23 +0800

    add appwrite nocodb and strapi scalfold

---
+++ strapi/strapi-v3/config/database.js
@@ -0,0 +1,19 @@
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'mysql',
+        host: env('DATABASE_HOST', '172.100.0.8'),
+        // host: env('DATABASE_HOST', '127.0.0.1'),
+        port: env.int('DATABASE_PORT', 3306),
+        database: env('DATABASE_NAME', 'strapi'),
+        username: env('DATABASE_USERNAME', 'root'),
+        password: env('DATABASE_PASSWORD', 'baidu123'),
+        ssl: env.bool('DATABASE_SSL', false),
+      },
+      options: {}
+    },
+  },
+});
